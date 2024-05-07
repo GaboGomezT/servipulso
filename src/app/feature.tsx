f6626ca0d6e700446e34cfdb4aac0f7f198522bd
GaboGomezT <gomezgabriel09@gmail.com>
2024-05-06 23:48:39 -0600

    added the how

--- src/app/feature.tsx
+++ src/app/feature.tsx
@@ -14,14 +14,14 @@ const Feature: React.FC<FeatureProps> = ({
 }) => {
   return (
     <div
-      className="flex flex-col gap-6 text-left max-w-72 md:items-start
-        items-center"
+      className="flex flex-col gap-6 max-w-72 md:items-start
+        items-center text-center"
     >
-      <div className="py-4 px-4 rounded-md border max-w-fit">
+      <div className="py-4 px-4 rounded-md border max-w-fit bg-emerald-400 text-white self-center">
         {icon}
       </div>
       <Typography variant="h3">{headline}</Typography>
-      <Typography variant="p" className="text-minor">
+      <Typography variant="p" className="text-minor text-left">
         {description}
       </Typography>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from '@/components/ui/button'
 import Typography from '@/components/ui/typography'
 import Image from 'next/image'
 import Feature from './feature'
-import { ArrowUpDown, Timer, Workflow } from 'lucide-react'
+import { MessageCircleQuestion, BarChart3, Vote } from 'lucide-react'
 import Link from 'next/link'
 import AnimatedLineChart from '@/components/charting/hero-line-chart'
 
@@ -45,24 +45,24 @@ export default function Home() {
       </div>
       <div className="flex flex-col md:pt-24 md:gap-36 gap-24 items-center">
         <div className="flex flex-col gap-12 items-center">
-          <Typography className="max-w-2xl" variant="h1">
-            Quick solutions, less stress
+          <Typography className="max-w-2xl underline" variant="h2">
+            ¿Cómo funciona?
           </Typography>
           <div className="flex md:flex-row flex-col gap-12">
             <Feature
-              icon={<Timer size={24} />}
-              headline="Fix emergencies fast"
-              description="Save 20-30 minutes per on-call ticket - no more searching for relevant issues and runbooks"
+              icon={<Vote size={24} />}
+              headline="Encuestas personalizables"
+              description="Personaliza tus encuestas con tu logo, colores para que se vean como parte de tu marca."
             />
             <Feature
-              icon={<ArrowUpDown size={24} />}
-              headline="Universally compatible"
-              description="Works with PagerDuty, Jira, or custom Slack alerts—Pandem integrates with any system"
+              icon={<MessageCircleQuestion size={24} />}
+              headline="Agrega tus propias preguntas"
+              description="Personaliza tus encuestas con preguntas abiertas o cerradas para obtener la información que necesitas."
             />
             <Feature
-              icon={<Workflow size={24} />}
-              headline="Secure for your org"
-              description="We keep your data safe by taking top security measures."
+              icon={<BarChart3 size={24} />}
+              headline="Gráficas en tiempo real y personalizables"
+              description="Visualiza los resultados de tus encuestas en gráficas interactivas y comprensibles. Usa las gráficas sugeridas o crea las tuyas."
             />
           </div>
         </div>
