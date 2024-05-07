5e4852bd6e9eb4679ac71f2962a1b39a1437744d
GaboGomezT <gomezgabriel09@gmail.com>
2024-05-06 22:29:31 -0600

    formatting

--- src/components/charting/hero-line-chart.tsx
+++ src/components/charting/hero-line-chart.tsx
@@ -1,12 +1,19 @@
-'use client';
+'use client'
 import {
-  AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer
-} from 'recharts';
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer
+} from 'recharts'
 
 // Define the type for the data array elements
 interface ChartData {
-  mes: string;    // Month in Spanish
-  satisfaccion: number; // Service satisfaction value
+  mes: string // Month in Spanish
+  satisfaccion: number // Service satisfaction value
 }
 
 // Sample data for the chart
@@ -17,24 +24,47 @@ const data: ChartData[] = [
   { mes: 'Abril', satisfaccion: 78 },
   { mes: 'Mayo', satisfaccion: 82 },
   { mes: 'Junio', satisfaccion: 85 }
-];
+]
 
 const AnimatedLineChart: React.FC = () => {
   return (
     <div className="w-full h-80 bg-white p-4 shadow-xl rounded-lg">
       <ResponsiveContainer width="100%" height="100%">
-        <AreaChart data={data}
-          margin={{ top: 10, right: 30, left: 20, bottom: 0 }}>
+        <AreaChart
+          data={data}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 20,
+            bottom: 0
+          }}
+        >
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="mes" />
-          <YAxis label={{ angle: -90, position: 'insideCenter' }} />
-          <Tooltip formatter={(value: number) => [`${value}%`, 'Satisfacción']} />
-          <Legend formatter={() => 'Satisfacción del servicio'} />
-          <Area type="monotone" dataKey="satisfaccion" stroke="#34D399" fill="#34D399"
-            activeDot={{ r: 8 }} animationDuration={2000} name="Satisfacción del servicio" />
+          <YAxis
+            label={{ angle: -90, position: 'insideCenter' }}
+          />
+          <Tooltip
+            formatter={(value: number) => [
+              `${value}%`,
+              'Satisfacción'
+            ]}
+          />
+          <Legend
+            formatter={() => 'Satisfacción del servicio'}
+          />
+          <Area
+            type="monotone"
+            dataKey="satisfaccion"
+            stroke="#34D399"
+            fill="#34D399"
+            activeDot={{ r: 8 }}
+            animationDuration={2000}
+            name="Satisfacción del servicio"
+          />
         </AreaChart>
       </ResponsiveContainer>
     </div>
-  );
+  )
 }
-export default AnimatedLineChart;
+export default AnimatedLineChart
