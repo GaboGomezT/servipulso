a7e16c50c847f036520706f1dbbc508373aa8025
GaboGomezT <gomezgabriel09@gmail.com>
2024-05-07 20:19:55 -0600

    removed email API

--- src/components/common/contact-form.tsx
+++ src/components/common/contact-form.tsx
@@ -1,14 +1,6 @@
 import { useState } from 'react';
 import { Button } from '@/components/ui/button';
 
-// import 'dotenv/config';
-// import { MailerSend, EmailParams, Sender, Recipient } from "mailersend";
-
-// const mailerSend = new MailerSend({
-//   apiKey: process.env.API_KEY || '',
-// });
-
-// const sentFrom = new Sender("gabriel@servipulso.com", "Gabriel Gómez");
 
 
 export default function ContactForm() {
@@ -17,20 +9,7 @@ export default function ContactForm() {
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    void (async () => {
-      // const recipients = [
-      //   new Recipient(email)
-      // ];
-
-      // const emailParams = new EmailParams()
-      //   .setFrom(sentFrom)
-      //   .setTo(recipients)
-      //   .setReplyTo(sentFrom)
-      //   .setSubject("Confirmación de lista de espera")
-      //   .setTemplateId('3z0vklo08y7l7qrx');
-
-      // await mailerSend.email.send(emailParams);
-    })();
+    
     setFormSubmitted(true);
   };
 
