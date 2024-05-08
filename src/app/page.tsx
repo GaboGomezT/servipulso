465a8d87f5d77fd98c493bfe9bcd18f7b807f413
GaboGomezT <gomezgabriel09@gmail.com>
2024-05-07 19:34:35 -0600

    added email capability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { Button } from '@/components/ui/button'
 import Typography from '@/components/ui/typography'
 import Feature from './feature'
--- src/components/common/contact-form.tsx
+++ src/components/common/contact-form.tsx
@@ -1,14 +1,34 @@
-'use client';
 import { useState } from 'react';
-import { Button } from '@/components/ui/button'
+import { Button } from '@/components/ui/button';
+
+import 'dotenv/config';
+import { MailerSend, EmailParams, Sender, Recipient } from "mailersend";
+
+const mailerSend = new MailerSend({
+  apiKey: process.env.API_KEY || '',
+});
+
+const sentFrom = new Sender("gabriel@servipulso.com", "Gabriel Gómez");
+
 
 export default function ContactForm() {
   const [formSubmitted, setFormSubmitted] = useState(false);
   const [email, setEmail] = useState('');
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(email); // replace this with your email sending logic
+    const recipients = [
+      new Recipient(email)
+    ];
+
+    const emailParams = new EmailParams()
+      .setFrom(sentFrom)
+      .setTo(recipients)
+      .setReplyTo(sentFrom)
+      .setSubject("Confirmación de lista de espera")
+      .setTemplateId('3z0vklo08y7l7qrx');
+
+    await mailerSend.email.send(emailParams);
     setFormSubmitted(true);
   };
 
@@ -29,6 +49,7 @@ export default function ContactForm() {
           <Button type="submit" size="tiny" variant="ghost" className='text-lg'>
             Entrar a la era de los datos
           </Button>
+          <span className='block my-4'>* Al enviar tu correo aceptas recibir nuestros correos de marketing</span>
         </form>
       )}
     </div>
